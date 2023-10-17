6689b5a665295c43f3235ffe0dcf709e8b3406ff
Phamducvinh <95126697+Phamducvinh@users.noreply.github.com>
2023-10-17 23:34:12 +0700

    Update login.js

--- VLife/assets/js/login.js
+++ VLife/assets/js/login.js
@@ -1,45 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const signUpForm = document.querySelector('.form-container.sign-up-container form');
-  
-    signUpForm.addEventListener('submit', function (e) {
-      e.preventDefault();
-  
-      // Lấy giá trị từ các trường nhập liệu
-      const name = signUpForm.querySelector('input[name="name"]').value;
-      const email = signUpForm.querySelector('input[name="email"]').value;
-      const password = signUpForm.querySelector('input[name="password"]').value;
-  
-      // Tạo dữ liệu JSON từ giá trị
-      const data = {
-        Name: name,
-        Email: email,
-        Password: password
-      };
-  
-      // Thực hiện yêu cầu POST tới sheetdb.io
-      fetch("https://sheetdb.io/api/v1/26iu7y7qm5xq4", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
+  const signUpForm = document.querySelector('.form-container.sign-up-container form');
+
+  signUpForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // Lấy giá trị từ các trường nhập liệu
+    const name = signUpForm.querySelector('input[name="name"]').value;
+    const email = signUpForm.querySelector('input[name="email"]').value;
+    const password = signUpForm.querySelector('input[name="password"]').value;
+
+    // Lấy ngày hôm nay
+    const today = new Date();
+    const day = today.getDate();
+    const month = today.getMonth() + 1; // Tháng trong JavaScript bắt đầu từ 0
+    const year = today.getFullYear();
+
+    // Tạo dữ liệu JSON từ giá trị
+    const data = {
+      Name: name,
+      Email: email,
+      Password: password,
+      Day: day,
+      Month: month,
+      Year: year
+    };
+
+    // Thực hiện yêu cầu POST tới sheetdb.io
+    fetch("https://sheetdb.io/api/v1/26iu7y7qm5xq4", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Xử lý kết quả sau khi gửi dữ liệu
+        console.log("Dữ liệu đã được gửi thành công:", data);
+        // Thêm thông báo "thành công"
+        alert("Đăng ký thành công!");
+        // Làm mới các trường nhập liệu
+        signUpForm.reset();
       })
-        .then(response => response.json())
-        .then(data => {
-             // Xử lý kết quả sau khi gửi dữ liệu
-            console.log("Dữ liệu đã được gửi thành công:", data);
-            // Thêm thông báo "thành công"
-            alert("Đăng ký thành công!");
-            // Làm mới các trường nhập liệu
-            signUpForm.reset();
-            
-        })
-        .catch(error => {
-          console.error("Lỗi khi gửi dữ liệu:", error);
-          // Xử lý lỗi nếu cần
-        });
-    });
+      .catch(error => {
+        console.error("Lỗi khi gửi dữ liệu:", error);
+        // Xử lý lỗi nếu cần
+      });
   });
+});
  // logn in
  document.addEventListener("DOMContentLoaded", function () {
     const signInButton = document.getElementById('signInButton');
